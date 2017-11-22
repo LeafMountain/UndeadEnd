8fc0ef52f69cfa4e0bc11b4a709241b8a877dcfe
Alexander Lövberg <lovbergalexander@gmail.com>
2017-11-22 22:22:15 +0100

    Tag system

---
+++ Assets/Scripts/Components/TagFilter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagFilter : MonoBehaviour {
+
+	public List<TagCheck> checks;
+
+	public void CheckTags(GameObject go){
+		Taggable taggable = go.GetComponent<Taggable>();
+
+		if(taggable){
+			for (int i = 0; i < checks.Count; i++){
+				checks[i].CheckTags(taggable);
+			}
+		}
+	}
+}
