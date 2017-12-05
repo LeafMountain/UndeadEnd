b8d4c97fc4e2a75895840ff1d76a772e7a8a1cb8
Alexander Lövberg <lovbergalexander@gmail.com>
2017-12-05 14:23:57 +0100

    Simple wander/idle AI

---
+++ Assets/Scripts/Components/AIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIController : MonoBehaviour {
+
+	IAIState currentState;
+	public NavMeshAgent agent;
+
+	public void ChangeState(IAIState newState){
+		currentState = newState;
+	}
+
+	void Start(){
+		agent = GetComponent<NavMeshAgent>();
+		currentState = new WanderState(this);
+	}
+
+	void Update(){
+		currentState.Update();
+	}
+}
