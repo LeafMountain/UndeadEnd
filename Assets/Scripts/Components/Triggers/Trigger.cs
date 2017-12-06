047b475674d7cc0714d2f19b8d7220e6b890c12e
Alexander Lövberg <lovbergalexander@gmail.com>
2017-12-06 14:23:09 +0100

    Weapon laser with color variable

---
+++ Assets/Scripts/Components/Triggers/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour {
+
+	public UnityGameObjectEvent OnTrigger;
+
+	public void Raise(GameObject go){
+		if(OnTrigger != null){
+			OnTrigger.Invoke(go);
+		}
+	}
+
+}
