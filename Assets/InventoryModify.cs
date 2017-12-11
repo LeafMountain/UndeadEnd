9b40debf2ae2c91a67f60459b323a139e15375bf
Alexander Lövberg <lovbergalexander@gmail.com>
2017-12-11 18:26:48 +0100

    InventoryModify component for adding, dropping and removing items from inventory

---
+++ Assets/InventoryModify.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryModify : MonoBehaviour {
+
+	public Inventory inventory;
+
+	public void Add(InventoryItem item){
+		//Check if inventory is full
+		inventory.AddItem(item);
+	}
+
+	public void Remove(InventoryItem item){
+		inventory.RemoveItem(item);
+	}
+
+	public InventoryItem Drop(InventoryItem item){
+		return item;
+	}
+}
